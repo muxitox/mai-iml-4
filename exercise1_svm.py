79c0dce28f3213e0217371cadf97c45591508991
Jesús Perales Hernández <jesusperalesh@MacBook-Pro-de-Jesus.local>
2018-12-23 12:35:16 +0100

    Primer ejercicio con primer dataset

--- exercise1_svm.py
+++ exercise1_svm.py
@@ -4,13 +4,11 @@
 
 
 import numpy as np
-#import cvxopt
-#import cvxopt.solvers
+import cvxopt
+import cvxopt.solvers
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
-import numpy as np
-
-
+from sklearn.metrics import accuracy_score
 
 
 if __name__ == "__main__":
@@ -70,61 +68,87 @@ if __name__ == "__main__":
         y_test = np.hstack((y1_test, y2_test))
         return X_test, y_test
 
+    def plot_hyperplane_and_support_vectors(clf, X_train, y_train):
+
+        plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, s=30, cmap=plt.cm.Paired)
+
+        # plot the decision function
+        ax = plt.gca()
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+
+        # create grid to evaluate model
+        xx = np.linspace(xlim[0], xlim[1], 30)
+        yy = np.linspace(ylim[0], ylim[1], 30)
+        YY, XX = np.meshgrid(yy, xx)
+        xy = np.vstack([XX.ravel(), YY.ravel()]).T
+        Z = clf.decision_function(xy).reshape(XX.shape)
+
+        # Plot hyperplane
+        ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1], alpha=0.5, linestyles=['--', '-', '--'])
+
+        # Plot support vectors
+        ax.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=100, linewidth=1, facecolors='none', edgecolors='k')
+
+        plt.show()
+
+
     def run_svm_dataset1():
         X1, y1, X2, y2 = generate_data_set1()
         X_train, y_train = split_train(X1, y1, X2, y2)
         X_test, y_test = split_test(X1, y1, X2, y2)
 
-         #### 
+         ####
         # Write here your SVM code and choose a linear kernel
         # plot the graph with the support_vectors_
         # print on the console the number of correct predictions and the total of predictions
         ####
-
-
+        # Linear
         clf = SVC(kernel='linear')
         clf.fit(X_train, y_train)
 
-        precision = clf.score(X_test, y_test)
+        y_pred = clf.predict(X_test)
+        print("SVM Linear")
+        print("Number of instances to predict:", len(X_test))
+        print("Number of instances correctly predicted:", accuracy_score(y_test, y_pred, normalize=False), "\n")
 
-        print('Precision ', precision);
+        plot_hyperplane_and_support_vectors(clf, X_train, y_train)
 
-        '''
-        Code for plotting from https://scikit-learn.org/stable/auto_examples/svm/plot_custom_kernel.html#sphx-glr-auto-examples-svm-plot-custom-kernel-py
-        '''
+        # RBF
+        clf = SVC(kernel='rbf', gamma='scale')
+        clf.fit(X_train, y_train)
 
-        h = .02
-        # Plot the decision boundary. For that, we will assign a color to each
-        # point in the mesh [x_min, x_max]x[y_min, y_max].
-        x_min, x_max = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
-        y_min, y_max = X_train[:, 1].min() - 1, X_train[:, 1].max() + 1
-        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        y_pred = clf.predict(X_test)
+        print("SVM RBF")
+        print("Number of instances to predict:", len(X_test))
+        print("Number of instances correctly predicted:", accuracy_score(y_test, y_pred, normalize=False), "\n")
 
-        plt.figure()
-        # Put the result into a color plot
-        Z = Z.reshape(xx.shape)
-        plt.pcolormesh(xx, yy, Z, cmap=   )
+        plot_hyperplane_and_support_vectors(clf, X_train, y_train)
 
-        # Plot also the training points
-        plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=plt.cm.Paired, edgecolors='k')
-        plt.title('Linear ')
-        plt.axis('tight')
-        plt.show()
+        # Sigmoid
+        clf = SVC(kernel='sigmoid', gamma='scale')
+        clf.fit(X_train, y_train)
+
+        y_pred = clf.predict(X_test)
+        print("SVM Sigmoid")
+        print("Number of instances to predict:", len(X_test))
+        print("Number of instances correctly predicted:", accuracy_score(y_test, y_pred, normalize=False), "\n")
+
+        plot_hyperplane_and_support_vectors(clf, X_train, y_train)
+
+
+
 
     def run_svm_dataset2():
         X1, y1, X2, y2 = generate_data_set2()
         X_train, y_train = split_train(X1, y1, X2, y2)
         X_test, y_test = split_test(X1, y1, X2, y2)
 
-        #### 
+        ####
         # Write here your SVM code and choose a linear kernel with the best C pparameter
         # plot the graph with the support_vectors_
         # print on the console the number of correct predictions and the total of predictions
         ####
-
-
-
 
 
 
@@ -133,8 +157,8 @@ if __name__ == "__main__":
         X_train, y_train = split_train(X1, y1, X2, y2)
         X_test, y_test = split_test(X1, y1, X2, y2)
 
-        #### 
-        # Write here your SVM code and use a gaussian kernel 
+        ####
+        # Write here your SVM code and use a gaussian kernel
         # plot the graph with the support_vectors_
         # print on the console the number of correct predictions and the total of predictions
         ####
@@ -145,10 +169,10 @@ if __name__ == "__main__":
 #############################################################
 #############################################################
 
-# EXECUTE SVM with THIS DATASETS      
+# EXECUTE SVM with THIS DATASETS
     run_svm_dataset1()   # data distribution 1
-    #run_svm_dataset2()   # data distribution 2
-    #run_svm_dataset3()   # data distribution 3
+    run_svm_dataset2()   # data distribution 2
+    run_svm_dataset3()   # data distribution 3
 
 #############################################################
 #############################################################
